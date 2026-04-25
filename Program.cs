e1f4144a086fd57c18cd2806eba507a7c7ff165f
thewarat <thewarat@imotif.io>
2026-04-26 02:52:31 +0700

    chore: add Swagger UI with root redirect and configure API documentation
    Add Swashbuckle.AspNetCore package, configure Swagger/SwaggerUI middleware for development environment, add endpoints API explorer service, and set up root path redirect to Swagger documentation page

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -12,12 +14,13 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+app.MapGet("/", () => Results.Redirect("/swagger"));
 app.MapControllers();
 
 app.Run();
